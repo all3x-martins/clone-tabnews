a8ccb265ec0fadcefc4a7a5da2c5ca8ff29401d3
all3x-martins <85321713+all3x-martins@users.noreply.github.com>
2025-02-05 22:34:20 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
-function Home () {
-    return <h1>Não importa quantas vezes você caia, o importante é se levantar e tentar de novo.</h1>
-
+function Home() {
+  return (
+    <h1>
+      Não importa quantas vezes você caia, o importante é se levantar e tentar
+      de novo.
+    </h1>
+  );
 }
 
 export default Home;
